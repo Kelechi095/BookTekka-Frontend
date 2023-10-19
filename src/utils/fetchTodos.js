f24017577366daaba0e5dcd909c832e5b4b5da66
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 12:26:29 +0100

    used axios to fetch

--- src/utils/fetchTodos.js
+++ src/utils/fetchTodos.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const fetchTodos = async () => {
-    const response = await fetch("import.meta.env.VITE_BASE_ENDPOINT/api/todos")
-    return response.json()
+    const response = await axios.get("import.meta.env.VITE_BASE_ENDPOINT/api/todos")
+    return response.data
   };
