b8ceb04f143773ea6a3168a303edde704cbc466a
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-28 17:54:52 +0100

    restyling home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,11 +17,11 @@ export default function Home() {
   return <div className="mx-auto bg-zinc-100 text-slate-900">
     <div className="bg-white p-6">
       <p className="font-semibold text-sm">Sort by</p>
-      <div className="mt-2 grid gap-4 grid-cols-2 text-xs">
-        <button className="border-2 border-blue-500 p-1 text-blue-600 bg-zinc-100 rounded">Task</button>
-        <button className="border p-1 bg-zinc-100 rounded">Date</button>
-        <button className="border p-1 bg-zinc-100 rounded">Date</button>
-        <button className="border p-1 bg-zinc-100 rounded">Order ID</button>
+      <div className="mt-2 grid gap-4 grid-cols-2 text-[13px]">
+        <button className="border-2 border-blue-500 p-[6px] text-blue-600 bg-zinc-100 rounded">Task</button>
+        <button className="border p-[6px] bg-zinc-100 rounded">Date</button>
+        <button className="border p-[6px] bg-zinc-100 rounded">Date</button>
+        <button className="border p-[6px] bg-zinc-100 rounded">Order ID</button>
       </div>
     </div>
   </div>;
