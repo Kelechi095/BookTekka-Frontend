7d846dbc36f1641512c1141ea43549bf280e29bb
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-30 18:49:59 +0100

    corrected styling error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ export default function Home() {
   if (isLoading) return <Loader />;
 
   return (
-    <div className={"mx-auto text-slate-900 py-6 px-4 relative"}>
+    <div className={"mx-auto text-slate-900 py-3 px-4 relative min-h-screen"}>
       {isOpen && <Sidebar toggleSidebar={toggleSidebar} />}
       <div>
         <div className="flex justify-between">
