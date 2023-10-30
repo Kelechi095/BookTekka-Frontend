b98e72020caf9b1a95a8bbf94b56a936b1eae8f8
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-30 22:02:10 +0100

    modified genre options

--- src/utils/buttons.js
+++ src/utils/buttons.js
@@ -1,9 +1,9 @@
 
   export const sortButtons = ['Date(Earliest first)', 'Date(Latest first)', 'Title (A-Z)', 'Title (Z-A)', 'Price (Highest first)', 'Price (Lowest first)']
 
-  export const filterGenres = ['African Literature', 'Fantasy', 'Horror', 'Mystery', 'Non Fiction', 'Romance',  'Science Fiction', 'Thriller', 'Young Adult']
+  export const filterGenres = ['African Literature', 'Fantasy', 'Horror', 'Literary Fiction', 'Mystery', 'Non Fiction', 'Romance',  'Science Fiction', 'Thriller', 'Young Adult', 'Others']
 
-  export const genreOptions = ["Genre", 'African Literature', 'Fantasy', 'Horror', 'Mystery', 'Non Fiction', 'Romance',  'Science Fiction', 'Thriller', 'Young Adult']
+  export const genreOptions = ["Genre", 'African Literature', 'Fantasy', 'Horror', 'Literary Fiction', 'Mystery', 'Non Fiction', 'Romance',  'Science Fiction', 'Thriller', 'Young Adult', 'Others']
 
   export const filterStatus = ["Unread", "Reading", "Finished"]
 
