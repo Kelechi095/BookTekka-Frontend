02e67da2f708eb6b916f3f3ce3d6a34b9920ee26
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-30 18:43:58 +0100

    modified loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Ring } from '@uiball/loaders'
-
+import React from "react";
+import { Waveform } from "@uiball/loaders";
 
 export default function Loader() {
   return (
-    <div className='h-screen flex items-center justify-center'>
-        <Ring />
+    <div className="h-screen flex justify-center">
+      <div className="mt-36">
+        <Waveform size={25}/>
+      </div>
     </div>
-  )
+  );
 }
