d46e422233c4c3260c81c4a880718923bd78d4f0
Awoms Kelechi <awomskelechi@gmail.com>
2023-11-10 23:59:35 +0100

    modified styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dItems } from "../utils/dashboardItems";
 
 export default function Dashboard() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (currentIndex === 2) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex((prev) => prev + 1);
+      }
+    }, 3000);
+
+    return () => clearInterval(id);
+  }, [currentIndex]);
+
+
   return (
-    <div className="@apply py-4 px-4 lg:px-12 lg:py-6 bg-lighterCyan h-screen">
+    <div className="py-4 px-4 lg:px-12 lg:py-6 bg-lighterCyan min-h-screen">
       <div>
-        <h2 className="mt-2 mx-auto max-w-md text-5xl md:text-6xl lg:text-7xl md:max-w-lg  lg:max-w-4xl text-center font-serif p-2 text-textColor">
+        <h2 className="mt-4 mx-auto max-w-md text-5xl md:text-6xl lg:text-7xl md:max-w-lg  lg:max-w-4xl text-center font-serif p-2 text-textColor">
           Welcome to BookTekka
         </h2>
       </div>
       <div className="mt-8">
         <img
-          src="https://res.cloudinary.com/djpyctzcq/image/upload/v1699640951/reading_green_jyckxt.svg"
+          src={dItems[currentIndex].image}
           alt=""
           className="w-40 mx-auto"
         />
-        <h4 className='text-md mt-4 font-mono max-w-md md:max-w-lg text-green-800 lg:max-w-3xl lg:text-xl text-center mx-auto p-3'>Keep track of your favorite books and meet fellow book lovers</h4>
-
+        <h4
+          className={
+            currentIndex === 0
+              ? "text-md mt-4 font-mono max-w-md md:max-w-lg text-green-500 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
+              : currentIndex === 1
+              ? "text-md mt-4 font-mono max-w-md md:max-w-lg text-red-500 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
+              : "text-md mt-4 font-mono max-w-md md:max-w-lg text-cyan-600 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
+          }
+        >
+          {dItems[currentIndex].text}
+        </h4>
       </div>
+
       <div className="mx-auto max-w-sm flex flex-col justify-center gap-4 mt-2">
-        <button className="border-green-800 text-md text-green-800 border  shadow-sm rounded-full py-[3px] px-2 w-full">Login</button>
-        <button className="border text-md shadow-sm rounded-full px-2 py-[3px] border-green-800 text-green-800 w-full">Register</button>
+        <button
+          className={
+            currentIndex === 0
+              ? "border-green-600 text-md text-green-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
+              : currentIndex === 1
+              ? "border-red-500 text-md text-red-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
+              : "border-blue-600 text-md text-blue-600 border  shadow-sm rounded-full py-[3px] px-2 w-full"
+          }
+        >
+          Login
+        </button>
+        <button
+          className={
+            currentIndex === 0
+              ? "border-green-600 text-md text-green-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
+              : currentIndex === 1
+              ? "border-red-600 text-md text-red-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
+              : "border-blue-600 text-md text-blue-600 border  shadow-sm rounded-full py-[3px] px-2 w-full"
+          }
+        >
+          Register
+        </button>
       </div>
     </div>
   );
---
+++ src/utils/dashboardItems.js
@@ -0,0 +1,16 @@
+export const dItems = [
+    {
+        image: "https://res.cloudinary.com/djpyctzcq/image/upload/v1699640951/reading_green_jyckxt.svg",
+        text: "Create a personal library to keep track of all your favorite books"
+    },
+    
+    {
+        image: "https://res.cloudinary.com/djpyctzcq/image/upload/v1699640949/meet_fellow_book_lovers_red_xhfgot.svg",
+        text: "Recommend your favorite books to others"
+    },
+    {
+        image: "https://res.cloudinary.com/djpyctzcq/image/upload/v1699640948/gir_reading_blue_ejdd3r.svg",
+        text: "Set reading goals and keep track of your reading progress"
+    },
+    
+]
