0108eec9a267726d875a2e28857eb23979272368
Awoms Kelechi <awomskelechi@gmail.com>
2023-11-11 07:39:32 +0100

    modified styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { dItems } from "../utils/dashboardItems";
 export default function Dashboard() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
+  /* useEffect(() => {
     const id = setInterval(() => {
       if (currentIndex === 2) {
         setCurrentIndex(0);
@@ -15,7 +15,7 @@ export default function Dashboard() {
 
     return () => clearInterval(id);
   }, [currentIndex]);
-
+ */
 
   return (
     <div className="py-4 px-4 lg:px-12 lg:py-6 bg-lighterCyan min-h-screen">
@@ -25,18 +25,10 @@ export default function Dashboard() {
         </h2>
       </div>
       <div className="mt-8">
-        <img
-          src={dItems[currentIndex].image}
-          alt=""
-          className="w-40 mx-auto"
-        />
+        <img src={dItems[currentIndex].image} alt="" className="w-40 mx-auto" />
         <h4
           className={
-            currentIndex === 0
-              ? "text-md mt-4 font-mono max-w-md md:max-w-lg text-green-500 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
-              : currentIndex === 1
-              ? "text-md mt-4 font-mono max-w-md md:max-w-lg text-red-500 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
-              : "text-md mt-4 font-mono max-w-md md:max-w-lg text-cyan-600 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
+            "text-md mt-4 font-mono max-w-md md:max-w-lg text-slate-800 lg:max-w-3xl lg:text-xl text-center mx-auto p-3"
           }
         >
           {dItems[currentIndex].text}
@@ -44,26 +36,10 @@ export default function Dashboard() {
       </div>
 
       <div className="mx-auto max-w-sm flex flex-col justify-center gap-4 mt-2">
-        <button
-          className={
-            currentIndex === 0
-              ? "border-green-600 text-md text-green-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
-              : currentIndex === 1
-              ? "border-red-500 text-md text-red-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
-              : "border-blue-600 text-md text-blue-600 border  shadow-sm rounded-full py-[3px] px-2 w-full"
-          }
-        >
+        <button className=" border border-slate-800 text-md text-slate-800  shadow-sm rounded-full py-[3px] px-2 w-full">
           Login
         </button>
-        <button
-          className={
-            currentIndex === 0
-              ? "border-green-600 text-md text-green-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
-              : currentIndex === 1
-              ? "border-red-600 text-md text-red-500 border  shadow-sm rounded-full py-[3px] px-2 w-full"
-              : "border-blue-600 text-md text-blue-600 border  shadow-sm rounded-full py-[3px] px-2 w-full"
-          }
-        >
+        <button className=" border border-slate-800 text-md text-slate-800  shadow-sm rounded-full py-[3px] px-2 w-full">
           Register
         </button>
       </div>
