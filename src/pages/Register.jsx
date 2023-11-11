6d21f9bd109e5ab636ce0ab4f48370c390cba7a8
Awoms Kelechi <awomskelechi@gmail.com>
2023-11-11 15:35:33 +0100

    modified styling

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,13 +5,11 @@ import { customFetch } from "../utils/customFetch";
 import { useMutation } from "react-query";
 import useIsLoggedIn from "../hooks/user/useIsLoggedIn";
 
-
 export default function Register() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
-  const {isLoggedIn} = useIsLoggedIn()
-
+  const { isLoggedIn } = useIsLoggedIn();
 
   const navigate = useNavigate();
 
@@ -42,60 +40,62 @@ export default function Register() {
   );
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    registerMutation()
-  }
+    e.preventDefault();
+    registerMutation();
+  };
 
   useEffect(() => {
-    if(isLoggedIn) navigate('/')
-  }, [isLoggedIn])
-
+    if (isLoggedIn) navigate("/");
+  }, [isLoggedIn]);
 
   return (
     <div className="p-3 max-w-sm mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Register</h1>
+      <h1 className="text-3xl font-bold my-8 ">Hello</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          id="username"
-          className="bg-slate-200 p-3 rounded"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          id="email"
-          className="bg-slate-200 p-3 rounded"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          className="bg-slate-200 p-3 rounded"
-          onChange={handleChange}
-        />
+        <div className="flex flex-col gap-2">
+          <label className="text-xs text-gray-600">Username</label>
+          <input
+            type="text"
+            id="username"
+            className="bg-white border-b border-purple-300 outline-none text-sm"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="text-xs text-gray-600">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="bg-white border-b border-purple-300 outline-none text-sm"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="text-xs text-gray-600">Username</label>
+          <input
+            type="password"
+            id="password"
+            className="bg-white border-b border-purple-300 outline-none text-sm"
+            onChange={handleChange}
+          />
+        </div>
         <button
-          className="bg-slate-700 text-white p-3 rounded-full hover:opacity-95 disabled:opacity-80"
+          className="bg-purple-800 text-white p-2 hover:opacity-95 disabled:opacity-80"
           disabled={isLoading}
         >
-          {isLoading ? "Submitting..." : "REGISTER"}
+          {isLoading ? "Submitting..." : "Register"}
         </button>
         {/* <Oauth /> */}
       </form>
-      <div className="flex gap-2 mt-5">
+      <div className="flex gap-2 mt-5 text-sm">
         <p>Have an account?</p>
         <Link to="/login">
-          <span className="text-blue-500">
+          <span className="text-purple-700">
             {isLoading ? "Submitting..." : "Login"}
           </span>
         </Link>
       </div>
       {error && <p className="text-red-700 mt-5">{customError}</p>}
-      <div className="">
-        <img src="https://res.cloudinary.com/djpyctzcq/image/upload/v1699638991/undraw_reading_re_29f8_h1cq6c.svg" alt="" className="w-48 mx-auto"/>
-      </div>
     </div>
   );
 }
