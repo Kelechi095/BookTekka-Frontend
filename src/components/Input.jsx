389be45fb3588f33e011aebb4710e04438fa02a9
Awoms Kelechi <awomskelechi@gmail.com>
2023-11-29 14:12:39 +0100

    modified form styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,9 @@
 "use client";
 import { BiDollar } from "react-icons/bi";
 
-
 const Input = ({
   id,
+  id1,
   label,
   type = "text",
   disabled,
@@ -12,51 +12,36 @@ const Input = ({
 }) => {
   return (
     <div className="w-full relative">
-      
-      <input
-        id={id}
-        disabled={disabled}
-        onChange={onChange}
-        placeholder=" "
-        type={type}
-        className={`
-          peer
-          w-full
-          p-1
-          pt-6 
-          pl-4 
-          bg-white 
-          border
-          border-neutral-400
-          rounded-md
-          outline-none
-          transition
-          disabled:opacity-70
-          disabled:cursor-not-allowed
-        `}
-      />
       <label
         className={`
-          absolute 
-          duration-150 
-          transform 
-          -translate-y-3
-          peer-first:
-          text-neutral-500 
-          top-5 
-          z-10 
-          origin-[0] 
-          left-4
-          peer-placeholder-shown:scale-100
-          peer-placeholder-shown:translate-y-0 
-          peer-focus:scale-75
-          peer-focus:-translate-y-4
-          peer-active:scale-75
+          pl-1
 
         `}
       >
         {label}
       </label>
+      <input
+        id={id}
+        disabled={disabled}
+        onChange={onChange}
+        placeholder={label.toLowerCase()}
+        type={type}
+        className={`
+          w-full
+          pl-2
+          py-3
+          font-light
+          bg-white 
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          "border-neutral-300"
+           "focus:border-black"
+        `}
+      />
     </div>
   );
 };
