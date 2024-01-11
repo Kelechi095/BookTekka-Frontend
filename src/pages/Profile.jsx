ceeb6d444b49d9ea3f8f69dd696880776503f9e8
Awoms Kelechi <awomskelechi@gmail.com>
2024-01-11 15:10:06 +0100

    fixed profile page display bug

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,26 +5,24 @@ import useGetUserProfile from "../hooks/user/useGetUserProfile";
 export default function Profile1() {
   const { user } = useGetUserProfile();
 
+  console.log(user)
+
   return (
     <div className="container">
       <div className="px-4">
         <Header title={"Profile"} />
         <div className="content">
-          <div className="max-w-sm mx-auto mt-24 flex flex-col items-center justify-center">
+          <div className=" mx-auto mt-8 flex flex-col items-center justify-center">
             <img
               src={user?.profilePicture}
               alt=""
-              className="shadow rounded-full w-40 h-40 object-cover border-none"
+              className="shadow rounded-full w-40 h-40 md:w-60 md:h-60 object-cover border-none"
             />
-            <p className="self-start mt-4">
-              <span className="font-semibold">Username:</span> {user?.username}
-            </p>
-            <p className="self-start mt-1">
-              <span className="font-semibold mb-4">Email:</span> {user?.email}
-            </p>
+            <p className="self-center mt-4 font-semibold md:text-lg">{user?.username}</p>
+            <p className="self-center mt-1 font-semibold">{user?.email}</p>
             <Link
               to="/profile/edit-profile"
-              className="border px-2 py-1 mt-1 rounded bg-cyan-600 text-white text-sm self-start"
+              className="border px-2 py-1 mt-1 rounded bg-cyan-600 text-white text-sm self-center"
             >
               Edit Profile
             </Link>
--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -19,34 +19,36 @@ export default function Profile1() {
 
   return (
     <div className="container">
-      
       <div className=" px-4">
         <Header title={"Profile"} />
-        <div className="content">
-          <div className="w-96 mx-auto flex flex-col items-center justify-center">
-          <img
-            src={data?.profilePicture}
-            alt=""
-            className="shadow rounded-full w-60 h-60 object-cover border-none mx-auto"
-          />
-          <p className="self-start mt-4">
-            <span className="font-semibold">Username:</span> {data?.username}
-          </p>
-          <p className="self-start mt-4">
-            <span className="font-semibold">Total books in library:</span>{" "}
-            {data?.totalBooks}
-          </p>
-          <p className="self-start mt-4">
-            <span className="font-semibold">Total books recommended:</span>{" "}
-            {data?.totalRecommendations}
-          </p>
+        <div className="content font-semibold">
+          <div className="mx-auto flex flex-col items-center justify-center">
+            <img
+              src={data?.profilePicture}
+              alt=""
+              className="shadow rounded-full w-60 h-60 object-cover border-none mx-auto"
+            />
+            <p className="self-center mt-4">{data?.username}</p>
+            <p className="self-center mt-4">
+              {data?.totalBooks} books in library
+            </p>
+            <p className="self-center mt-4">
+            {data?.totalRecommendations} books recommended
+              
+            </p>
 
-          <p className="self-start mt-4 font-semibold">Recently added</p>
-          <div className="flex gap-3 self-start">
-            {data?.recentlyAdded?.slice(0, 3).map((book) => (
-              <img src={book.thumbnail} alt="" className="border rounded w-20 mt-2" />
-            ))}
-          </div>
+            <p className="self-center mt-4 font-semibold underline text-cyan-500">
+              Recently added
+            </p>
+            <div className="flex gap-3 self-center mb-4">
+              {data?.recentlyAdded?.slice(0, 3).map((book) => (
+                <img
+                  src={book.thumbnail}
+                  alt=""
+                  className="border rounded w-20 mt-2"
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
